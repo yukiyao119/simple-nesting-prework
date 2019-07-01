9afe35b22b5700d77db971546afdae07e4f7b6ec
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-01 20:39:46 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,11 +58,6 @@ def dennis_ritchies_language
 end
 
 def adding_matz
-# add the following information to the top level of programmer_hash
-# :yukihiro_matsumoto => {
-#   :known_for => "Ruby",
-#    :languages => ["LISP, C"]
-# }
 
 	programmer_hash = 
  		{
@@ -80,6 +75,10 @@ def adding_matz
         }
      }
 
+  programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
+  programmer_hash[:languages] = ["LISP, C"]
+  
+  programmer_hash
     
 end
 
@@ -102,6 +101,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
+     programmer_hash[:alan_kay][:known_for] = alans_new_info
+     
+     programmer_hash
      
 end
 
