0335ec8b2e2bbc259b9ffcac0e014bc32902ebe1
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-07-01 20:43:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -78,7 +78,7 @@ def adding_matz
 
   programmer_hash[:yukihiro_matsumoto]
   programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-  programmer_hash[:languages] = ["LISP, C"]
+  programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP, C"]
   
   programmer_hash
     
